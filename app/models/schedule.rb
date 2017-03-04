760cdec697a533f5535b4545fc9024a392f0d275
Raghu Betina <raghu@firstdraft.com>
2017-03-04 21:32:14 +0000

    Direct association between teams and schedules

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,9 @@
 class Schedule < ApplicationRecord
   # Direct associations
 
+  belongs_to :away_team,
+             :class_name => "Team"
+
   belongs_to :home_team,
              :class_name => "Team"
 
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,11 @@
 class Team < ApplicationRecord
   # Direct associations
 
+  has_many   :away_events,
+             :class_name => "Schedule",
+             :foreign_key => "away_team_id",
+             :dependent => :destroy
+
   has_many   :home_events,
              :class_name => "Schedule",
              :foreign_key => "home_team_id",
